1e80b84e1ee6273aa5c4144b9da1fa262dcefe0f
jingwei <jingwei269@gmail.com>
2025-05-09 22:33:14 +0800

    Net

--- Nft_Sec_Network-Policy/verify.sh
+++ Nft_Sec_Network-Policy/verify.sh
@@ -22,7 +22,7 @@ echo "[INFO] Accessing same-project service..."
 oc rsh "$POD" curl --max-time 5 http://network-policy-test.network-policy-2.svc.cluster.local:8080 | head -10
 
 echo "[INFO] Accessing cross-project service, should fail..."
-if oc rsh "$POD" curl --max-time 5 http://httpd.network-policy-1.svc.cluster.local:8080 |head -n 10; then
+if oc rsh "$POD" curl --max-time 5 http://httpd.network-policy-1.svc.cluster.local:8080 ; then
   log_error "Cross-project traffic unexpectedly succeeded"
   exit 1
 else
